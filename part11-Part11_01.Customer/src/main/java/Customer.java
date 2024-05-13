236abc86d0312748939356646aa68297cb4ab6dc
cankurttekin <cankurttekin@gmail.com>
2024-05-13 13:33:48 +0300

    uml

---
+++ part11-Part11_01.Customer/src/main/java/Customer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author can
+ */
+public class Customer {
+    // UML class diagram of this exercise: https://java-programming.mooc.fi/a7b59abdd2feaf966091b910477a065f/class-diagram-customer.svg
+    
+    private String name;
+    private String address;
+    private String email;
+    
+    
+}
